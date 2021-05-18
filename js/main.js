cae16f49b3370caf290cb6d44c0315010fc4592c
Ahmed Ghonim <ahmed.ghonim.dr.cs@gmail.com>
2021-05-18 06:51:07 +0200

    edit4

--- js/main.js
+++ js/main.js
@@ -1,64 +1,48 @@
 
-$('.fa-bars').click(function () {
-    if ($(".SCmenu").css("height") == "0px") {
+let wid = $(".SCmenu").css("height")
+let start_zero = _ => {
+    $(".SCmenu").css("height") == wid ? $(".SCmenu").animate({ "height": "0px" }, function () {
+        $(".fa-running").fadeOut(1000)
+        $(".fa-palette").fadeOut(1000)
+    }) : $(".SCmenu").animate({ "height": `${wid}` }, function () {
+        $(".fa-running").fadeIn()
+        $(".fa-palette").fadeIn()
+    })
+}
+// start_zero()
+$('.fa-bars').click(start_zero);
 
-        $(".SCmenu").css({ "height": "90px" })
+let wid2 = $('.range-slider').css("width")
+let startRingZero = _ => {
+    if ($('.range-slider').css("width") == "0px") {
+        $(".range-slider ").animate({ width: `${wid2}` })
+        $(".colorcontainer ").animate({ width: `0px` })
     } else {
-        $(".SCmenu").animate({ height: "0px" })
+        $(".range-slider").animate({ width: `0px` },function () {
+            
+        })
+    }
+
+}
+startRingZero()
+$(".fa-running").click(startRingZero);
+
+
+let wid3 = $('.colorcontainer').css("width")
+let strtColorZero = _ => {
+
+    if ($('.colorcontainer').css("width") == `${wid3}`) {
+        $(".colorcontainer ").animate({ width: `0px` })
+    } else {
+        $(".colorcontainer").animate({ width: `${wid3}` })
+        $(".range-slider").animate({ width: `0px` })
+        
 
     }
 
-});
-
-$(".fa-running").click(function () {
-    let wid = $('.range-slider').innerWidth()
-
-    console.log(wid);
-    if ($('.sid-mnu').css("right") == "0px") {
-        console.log(wid);
-        $(".sid-mnu").animate({ right: `-${wid}px` }, function () {
-
-            $("input").fadeOut(1000);
-        })
-
-    } else {
-
-        console.log(wid);
-
-        $(".sid-mnu ").animate({ right: `${0}` }, function () {
-
-            $("input").fadeIn(1000);
-
-        })
-
-    }
-
-});
-
-$(".fa-palette").click(function () {
-    let wid = $('.colorcontainer').innerWidth()
-
-    if ($('.sid-mnu').css("right") == "0px") {
-
-        console.log(wid);
-        $(".sid-mnu").animate({ right: `-${wid}px` }, function () {
-
-            $(".colorBox").fadeOut(1000);
-        })
-
-    } else {
-
-        console.log(wid);
-
-        $(".sid-mnu ").animate({ right: `${0}` }, function () {
-
-            $(".colorBox").fadeIn(1000);
-
-        })
-
-    }
-
-});
+}
+strtColorZero()
+$(".fa-palette").click(strtColorZero);
 // (function () {
 // //     $(".colorBox").fadeOut(1000);
 // //     $("input").fadeOut(1000);
